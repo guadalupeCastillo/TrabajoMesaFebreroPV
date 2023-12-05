86be3e3921e714b4aa649de2595d21de5c4ae808
BolanhoAlejo <alejobolanho@gmail.com>
2023-12-05 18:29:29 -0300

    Added punto de interés view

--- src/main/java/ar/edu/unju/edm/controlador/PuntoDeInteresControlador.java
+++ src/main/java/ar/edu/unju/edm/controlador/PuntoDeInteresControlador.java
@@ -6,14 +6,18 @@ import ar.edu.unju.edm.servicio.UsuarioServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
 
-@RestController
+@Controller
 public class PuntoDeInteresControlador {
     @Autowired
     private UsuarioServicio usuarioServicio;
@@ -24,14 +28,22 @@ public class PuntoDeInteresControlador {
     private final Logger logger = LoggerFactory.getLogger(PuntoDeInteresControlador.class);
 
     @GetMapping("/")
-    public ModelAndView mostrarPuntosDeInteres() {
-        var model = new ModelAndView("index");
-        model.addObject("pois", puntoDeInteresServicio.buscarTodos());
-        return model;
+    public String mostrarPuntosDeInteres(Model model) {
+        model.addAttribute("pois", puntoDeInteresServicio.buscarTodos());
+        return "index";
     }
 
+    @GetMapping("/poi/{id}")
+    public String mostrarPuntoDeInteresPorId(@PathVariable Long id, Model model) {
+        var poi = puntoDeInteresServicio.buscarPorId(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("poi", poi);
+        model.addAttribute("usuarioId", usuarioServicio.obtenerUsuarioActual().orElseThrow().getId());
+        return "/puntoDeInteres";
+    }
+
+
     @PostMapping("/poi")
-//    @ResponseBody
+    @ResponseBody
     public PuntoDeInteres agregarPoi(@Valid @RequestBody PuntoDeInteres poi, BindingResult result) {
         poi.setCreador(usuarioServicio.obtenerUsuarioActual().orElseThrow());
         logger.info(poi.toString());
